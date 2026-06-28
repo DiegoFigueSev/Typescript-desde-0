4a216a97be80da7aae79b06c21fd1a367825e3a6
DiegoFigueSev <diego.figueroa.sevillano.dev@outlook.com>
2026-06-28 14:25:41 -0400

    feat: final version

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 (() => {
 
-    //? En este punto vamos a ver lo que son funciones y objetos
 
-    //* 
+    const valuesString:string = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
+    let valuesMap:{ [key: string]: number } = {}
+    valuesString.split(' ').forEach((e, i) => valuesMap[e]=i+1)
+    console.log('a' in valuesMap);
 
+    function alphabetPosition(text:string):string {
+        let response:string = '';
+        for (let val of text){
+            if (val !== ' ' || val.toLowerCase() in valuesMap){
+                response += `${valuesMap[val.toLowerCase()]} `
+            }
+        }
+        return response.trim();
+    }
+    
+    console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+    
 })()
---
+++ src/tema_03_colecciones/clase_01_arrays.ts
@@ -0,0 +1,14 @@
+(() => {
+
+    //* Que es un array? 
+    // Un array en ts/js no es mas que una lista simple en otros lenguajes
+    // List<> en caso de JAVA 
+
+    // Un array es mutable, iterable, ordenado, etc
+
+    //? [tipo de variable] [nombre] : {tipo de dato del array}[]
+    let apellidos:string[] = [];
+
+    apellidos.push('hola')
+
+})()
